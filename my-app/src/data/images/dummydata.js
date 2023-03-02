a5d573d8e102238b8c026c4594916202957b15fc
ilike3p <39431924+ilike3p@users.noreply.github.com>
2023-03-02 04:39:59 -0800

    updated css

--- my-app/src/data/images/dummydata.js
+++ my-app/src/data/images/dummydata.js
@@ -34,8 +34,204 @@ export const home = [
   {
     text: "Hello, My Name is",
     name: "Tray (3P) Pearce",
-    post: "WEB DESIGNER",
+    post: "Full Stack Developer",
     design: "UI / UX DESIGNER",
-    desc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book",
+    desc: "Recent UCLA bootcamp graduate that has a passion for building dynamic and user-friendly web applications. I enjoy bringing creative designs to life",
   },
 ]
+export const about = [
+    {
+      desc: "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur",
+      desc1: "magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor si voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur",
+      cover: "./images/man.png",
+    },
+  ]
+  export const services = [
+    {
+      id: 1,
+      icon: <Settings />,
+      title: "Creative Design",
+      desc: "Lorem Ipsum simply text of the printing and type setting industry when an unknown printing simply",
+    },
+    {
+      id: 2,
+      icon: <CropRotate />,
+      title: "Clean Code",
+      desc: "Lorem Ipsum simply text of the printing and type setting industry when an unknown printing simply",
+    },
+    {
+      id: 3,
+      icon: <ViewInAr />,
+      title: "Responsive Design",
+      desc: "Lorem Ipsum simply text of the printing and type setting industry when an unknown printing simply",
+    },
+    {
+      id: 4,
+      icon: <PieChart />,
+      title: "Material UI	",
+      desc: "Lorem Ipsum simply text of the printing and type setting industry when an unknown printing simply",
+    },
+    {
+      id: 5,
+      icon: <Code />,
+      title: "Material UI Icons",
+      desc: "Lorem Ipsum simply text of the printing and type setting industry when an unknown printing simply",
+    },
+    {
+      id: 6,
+      icon: <BarChart />,
+      title: "Awesome Support",
+      desc: "Lorem Ipsum simply text of the printing and type setting industry when an unknown printing simply",
+    },
+  ]
+  export const project = [
+    {
+      id: 1,
+      icon: <CloudOutlined />,
+      num: "001",
+      title: "HAPPY CLIENTS",
+    },
+    {
+      id: 2,
+      icon: <FavoriteBorder />,
+      num: "30",
+      title: "PROJECTS COMPLEATED",
+    },
+    {
+      id: 3,
+      icon: <Public />,
+      num: "10000000",
+      title: "FILES DOWNLOADED",
+    },
+    {
+      id: 4,
+      icon: <PersonOutlined />,
+      num: "1446",
+      title: "LIENS OF CODE",
+    },
+  ]
+  export const portfolio = [
+    {
+      id: 1,
+      cover: "",
+      name: "Brand",
+      category: "marketing",
+      title: "Brex Logo",
+    },
+    {
+      id: 2,
+      cover: "../images/port/port2.jpg",
+      name: "Brand",
+      category: "design",
+      title: "Brex Logo",
+    },
+    {
+      id: 3,
+      cover: "../images/port/port3.jpg",
+      name: "Brand",
+      category: "development",
+      title: "Brex Logo",
+    },
+    {
+      id: 4,
+      cover: "../images/port/port4.jpg",
+      name: "Brand",
+      category: "marketing",
+      title: "Brex Logo",
+    },
+    {
+      id: 5,
+      cover: "../images/port/port5.jpg",
+      name: "Brand",
+      category: "design",
+      title: "Brex Logo",
+    },
+    {
+      id: 6,
+      cover: "../images/port/port6.jpg",
+      name: "Brand",
+      category: "development",
+      title: "Brex Logo",
+    },
+  ]
+  export const testimonials = [
+    {
+      id: 1,
+      text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam porttitordapibus dictum.Fusce faucibus ligula scelerisque, eleifend turpis in",
+      image: "./images/testimonials/team-1.png",
+      name: "Tray Pearce",
+      post: "Full Stack Developer",
+    },
+    {
+      id: 2,
+      text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam porttitordapibus dictum.Fusce faucibus ligula scelerisque, eleifend turpis in",
+      image: "./images/testimonials/team-2.png",
+      name: "Alex Ander",
+      post: "Back End Developer",
+    },
+    {
+      id: 3,
+      text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam porttitordapibus dictum.Fusce faucibus ligula scelerisque, eleifend turpis in",
+      image: "./images/testimonials/team-3.png",
+      name: "GorkCoder",
+      post: "React Developer",
+    },
+  ]
+  export const blog = [
+    {
+      id: 1,
+      title: "Master These Awesome",
+      date: "Jun 27, 2022",
+      author: "Dorian Gray",
+      desc: "Lorem Ipsum has been standard. Lorem Ipsum is simply text of the printing and typesetting industry. Lorem Ipsum has been",
+      cover: "./images/blog/b1.png",
+    },
+    {
+      id: 2,
+      title: "Best Design Items to Appeal",
+      date: "Jun 27, 2022",
+      author: "Dorian Gray",
+      desc: "Lorem Ipsum has been standard. Lorem Ipsum is simply text of the printing and typesetting industry. Lorem Ipsum has been",
+      cover: "./images/blog/b2.png",
+    },
+    {
+      id: 3,
+      title: "The 20 Best Lightroom Presets",
+      date: "Jun 27, 2022",
+      author: "Dorian Gray",
+      desc: "Lorem Ipsum has been standard. Lorem Ipsum is simply text of the printing and typesetting industry. Lorem Ipsum has been",
+      cover: "./images/blog/b3.png",
+    },
+  ]
+  export const contact = [
+    {
+      icon: <AddLocationAltOutlined />,
+      text1: "1030 Orchard Drive",
+      text2: "Inglewood, CA, 90301",
+    },
+    {
+      icon: <PhoneIphone />,
+      text1: "323-535-2164",
+      text2: "0345627891",
+    },
+    {
+      icon: <EmailOutlined />,
+      text1: "traypearce@gmail.com",
+      text2: "traypearce@yahoo.com",
+    },
+  ]
+  export const social = [
+    {
+      icon: <Facebook />,
+    },
+    {
+      icon: <Twitter />,
+    },
+    {
+      icon: <Instagram />,
+    },
+    {
+      icon: <YouTube />,
+    },
+  ]
+  
