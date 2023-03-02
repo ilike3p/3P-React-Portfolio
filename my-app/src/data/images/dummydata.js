4b564ea21cf88c2a6d4905e257ddf19156caab2a
ilike3p <39431924+ilike3p@users.noreply.github.com>
2023-03-02 04:09:37 -0800

    update data

--- my-app/src/data/images/dummydata.js
+++ my-app/src/data/images/dummydata.js
@@ -1,4 +1,41 @@
-import "./App.css"
-import { Pages } from "./components/pages/Pages"
-import { useEffect } from "react"
+import { Settings, CropRotate, ViewInAr, PieChart, Code, BarChart, CloudOutlined, FavoriteBorder, Public, PersonOutlined, AddLocationAltOutlined, PhoneIphone, EmailOutlined, Facebook, Twitter, Instagram, YouTube } from "@mui/icons-material"
 
+export const navlink = [
+  {
+    url: "/",
+    text: "Home",
+  },
+  {
+    url: "/about",
+    text: "About",
+  },
+  {
+    url: "/services",
+    text: "Services",
+  },
+  {
+    url: "/portfolio",
+    text: "Portfolio",
+  },
+  {
+    url: "/testimonials",
+    text: "Testimonials",
+  },
+  {
+    url: "/blog",
+    text: "Blog",
+  },
+  {
+    url: "/contact",
+    text: "Contact",
+  },
+]
+export const home = [
+  {
+    text: "Hello, My Name is",
+    name: "Tray (3P) Pearce",
+    post: "WEB DESIGNER",
+    design: "UI / UX DESIGNER",
+    desc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book",
+  },
+]
